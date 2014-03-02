3a426fd3c4247fffcc621f147289d7d7e7dca4be
androidand <tantonet@gmail.com>
2014-03-02 23:12:33 +0100

    Intet nytt
    Inget speciellt.

--- js/view/fullRecipeView.js
+++ js/view/fullRecipeView.js
@@ -1,14 +1,20 @@
 var FullRecipeView = function(container,main, model){
 	this.container = container;
-    	$(container).hide();
+    	//$(container).hide();
 
-    	this.btn =  $('testBtn');
+    	//this.btn =  $('testBtn');
+
+    	dishes = model.getFullMenu();
+
+    	// I denna container bör skapas en container per dish.
+    	var fullRecipeView = container.find("#fullRecipeView");
 
     	//Vi vill ju ha getFullMenu som i Cart...
+    	fullRecipeView.innerHTML("TEEEEEEEEEEEEST!");
 
 
 		//Fast visa det som i displayDish....
 
-		
+
 
 	};
